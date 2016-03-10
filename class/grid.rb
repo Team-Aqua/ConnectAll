0386acefcd00f1fc910cd45700503e9044643f05
anson-li <ansonli.94@gmail.com>
2016-03-10 13:32:51 -0700

    initial winton built

--- class/grid.rb
+++ class/grid.rb
@@ -11,8 +11,7 @@ class Grid
 
   def win(state)
     # check horizontal, vertical, diagonal
-    winstate = check_vertical(state) or check_horizontal(state) or check_diagonal(state)
-    puts winstate
+    winstate = check_vertical(state) || check_horizontal(state) || check_diagonal(state)
     return winstate
   end
 
@@ -39,7 +38,7 @@ class Grid
         (0..7).each { |x|
           if y < 5 and @grid[y][x] == 1
             winstate = 1
-            (y..7).each { |yitr|
+            (y..y + 3).each { |yitr|
               if @grid[yitr][x] != 1
                 winstate = 0
               end
@@ -55,10 +54,134 @@ class Grid
   end
 
   def check_horizontal(state)
+    winstate = 0
+    if state == 0 # green, 2
+      (0..7).each { |y|
+        (0..7).each { |x|
+          if x < 5 and @grid[y][x] == 2
+            winstate = 1
+            (x..x + 3).each { |xitr|
+              if @grid[y][xitr] != 2
+                # puts "Failed at: #{xitr} with result: #{@grid[y][xitr]}" 
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+          end  
+        }
+      }
+    else # purple, 1
+      (0..7).each { |y|
+        (0..7).each { |x|
+          if x < 5 and @grid[y][x] == 1
+            winstate = 1
+            (x..x + 3).each { |xitr|
+              if @grid[y][xitr] != 1
+                # puts "Failed at: #{xitr} with result: #{@grid[y][xitr]}" 
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+          end  
+        }
+      }
+    end
     return false
   end
 
   def check_diagonal(state)
+    winstate = 0
+    if state == 0 # green, 2
+      (0..7).each { |y|
+        (0..7).each { |x|
+          if x < 5 and y < 5 and x > 2 and y > 2 and @grid[y][x] == 2
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y + itr][x + itr] != 2
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y + itr][x - itr] != 2
+                winstate = 0
+              end
+            }
+            if winstate == 1
+              return true
+            end  
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y - itr][x + itr] != 2
+                winstate = 0
+              end
+            }
+            if winstate == 1
+              return true
+            end
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y - itr][x - itr] != 2
+                winstate = 0
+              end
+            }
+            if winstate == 1
+              return true
+            end    
+          end  
+        }
+      }
+    else # purple, 1
+      (0..7).each { |y|
+        (0..7).each { |x|
+          if x < 5 and y < 5 and x > 2 and y > 2 and @grid[y][x] == 1
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y + itr][x + itr] != 1
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y + itr][x - itr] != 1
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y - itr][x + itr] != 1
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+            winstate = 1
+            (1..3).each { |itr|
+              if @grid[y - itr][x - itr] != 1
+                winstate = 0
+              end
+            }  
+            if winstate == 1
+              return true
+            end
+          end  
+        }
+      }
+    end
     return false
   end
 
