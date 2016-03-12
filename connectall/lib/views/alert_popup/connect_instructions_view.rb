4102204a5b2681805e31855868c1d6ed0caea6a5
anson-li <ansonli.94@gmail.com>
2016-03-12 15:03:34 -0700

    assets

---
+++ connectall/lib/views/alert_popup/connect_instructions_view.rb
@@ -0,0 +1,24 @@
+module Views
+  class ClassicInstructionsAlertView
+
+    def initialize(window, controller)
+      @window = window
+      @controller = controller
+      @help = Gosu::Image.new("assets/images/item_what_is_classic_mode.png", :tileable => false)
+      @cancel = BtnItem.new(@window, Gosu::Image.new("assets/images/btn_cancel_dark.png"), 290, 20, 100, lambda { @controller.alert_close }, Gosu::Image.new("assets/images/btn_cancel_dark.png", :tileable => false))
+    end
+
+    def draw
+      @help.draw(30, 10, 100)
+      @cancel.draw
+    end
+    
+    def update
+      @cancel.update
+    end
+    
+    def clicked
+      @cancel.clicked
+    end
+
+  end
