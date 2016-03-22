043612f60d42c848ed3ae73216fbd0b42e4e8bdf
anplamon <anplamon@ualberta.ca>
2016-03-22 16:42:35 -0600

    Finished classic ai.

--- connectall/lib/controllers/game_logic/classic_AI.rb
+++ connectall/lib/controllers/game_logic/classic_AI.rb
@@ -31,7 +31,7 @@ module GameLogic
         end
         
         # Check Horizontal
-        y = @model::grid.column_depth(x+1)-1
+        y = @model::grid.column_depth(x+1) - 1
         if x < 5 
           if @grid[y][x+1] == 2 and @grid[y][x+2] == 2 and @grid[y][x+3] == 2
             return x+1
@@ -50,14 +50,63 @@ module GameLogic
           end
         end
         
-        
         if x > 2 
           if @grid[y][x-3] == 2 and @grid[y][x-2] == 2 and @grid[y][x-1] == 2
             return x+1
           end
         end
+        
+        # Check Diagonal
+        y = @gridModel.column_depth(x+1)
+        
+        if y > 3 and x < 5
+          if @grid[y - 2][x + 1] == 2 and @grid[y - 3][x + 2] == 2 and @grid[y - 4][x + 3] == 2
+            return x+1
+          end
+        end
+        
+        if y < 6 and x < 5
+          if @grid[y][x + 1] == 2 and @grid[y + 1][x + 2] == 2 and @grid[y + 2][x + 3] == 2
+            return x+1
+          end
+        end
+        
+        if y > 2 and y < 8 and x < 6 and x > 0
+          if @grid[y][x - 1] == 2 and @grid[y - 2][x + 1] == 2 and @grid[y - 3][x + 2] == 2
+            return x+1
+          end
+        end
+        
+        if y > 1 and y < 7 and x < 6 and x > 1
+          if @grid[y - 2][x - 1] == 2 and @grid[y][x + 1] == 2 and @grid[y + 1][x + 2] == 2
+            return x+1
+          end
+        end
+        
+        if y > 1 and y < 7 and x < 7 and x > 1
+          if @grid[y + 1][x - 2] == 2 and @grid[y][x - 1] == 2 and @grid[y - 2][x + 1] == 2
+            return x+1
+          end
+        end
+        
+        if y > 2 and y < 8 and x < 7 and x > 1
+          if @grid[y - 3][x - 2] == 2 and @grid[y - 2][x - 1] == 2 and @grid[y][x + 1] == 2
+            return x+1
+          end
+        end
+        
+        if y <= 5 and x >= 3
+          if @grid[y + 2][x - 3] == 2 and @grid[y + 1][x - 2] == 2 and @grid[y][x - 1] == 2
+            return x+1
+          end
+        end
+        
+        if y > 3 and x >= 3
+          if @grid[y - 4][x - 3] == 2 and @grid[y - 3][x - 2] == 2 and @grid[y-2][x - 1] == 2
+            return x+1
+          end
+        end
       }
-      
       
       # Check if Player can win.
       y = 0
@@ -71,8 +120,8 @@ module GameLogic
         end
         
         # Check Horizontal
-        y = @gridModel.column_depth(x+1)-1
-        if x < 5 
+        y = @gridModel.column_depth(x+1) - 1
+        if x < 5
           if @grid[y][x+1] == 1 and @grid[y][x+2] == 1 and @grid[y][x+3] == 1
             return x+1
           end
@@ -90,13 +139,64 @@ module GameLogic
           end
         end
         
-        
-        if x > 2 
+        if x > 2
           if @grid[y][x-3] == 1 and @grid[y][x-2] == 1 and @grid[y][x-1] == 1
             return x+1
           end
         end
+        
+        # Check Diagonal
+        y = @gridModel.column_depth(x+1)
+        
+        if y > 3 and x < 5
+          if @grid[y - 2][x + 1] == 1 and @grid[y - 3][x + 2] == 1 and @grid[y - 4][x + 3] == 1
+            return x+1
+          end
+        end
+
+        if y < 6 and x < 5
+          if @grid[y][x + 1] == 1 and @grid[y + 1][x + 2] == 1 and @grid[y + 2][x + 3] == 1
+            return x+1
+          end
+        end
+
+        if y > 2 and y < 8 and x < 6 and x > 0
+          if @grid[y][x - 1] == 1 and @grid[y - 2][x + 1] == 1 and @grid[y - 3][x + 2] == 1
+            return x+1
+          end
+        end
+
+        if y > 1 and y < 7 and x < 6 and x > 1
+          if @grid[y - 2][x - 1] == 1 and @grid[y][x + 1] == 1 and @grid[y + 1][x + 2] == 1
+            return x+1
+          end
+        end
+
+        if y > 1 and y < 7 and x < 7 and x > 1
+          if @grid[y + 1][x - 2] == 1 and @grid[y][x - 1] == 1 and @grid[y - 2][x + 1] == 1
+            return x+1
+          end
+        end
+        
+        if y > 2 and y < 8 and x < 7 and x > 1
+          if @grid[y - 3][x - 2] == 1 and @grid[y - 2][x - 1] == 1 and @grid[y][x + 1] == 1
+            return x+1
+          end
+        end
+
+        if y <= 5 and x >= 3
+          if @grid[y + 2][x - 3] == 1 and @grid[y + 1][x - 2] == 1 and @grid[y][x - 1] == 1
+            return x+1
+          end
+        end
+        
+        if y > 3 and x >= 3
+          if @grid[y - 4][x - 3] == 1 and @grid[y - 3][x - 2] == 1 and @grid[y-2][x - 1] == 1
+            return x+1
+          end
+        end
       }
+      
       
       # No Better Choice
       rando = Random.new
