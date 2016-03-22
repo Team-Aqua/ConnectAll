b4a796fcba657e65487be6ce795e0c7ce0c7a490
anson-li <ansonli.94@gmail.com>
2016-03-22 17:31:47 -0600

    wait

--- connectall/lib/contracts/menu_controller_contracts.rb
+++ connectall/lib/contracts/menu_controller_contracts.rb
@@ -5,16 +5,16 @@ module MenuControllerContracts
     end
   end
 
-  def invariant(game_state_model)
+  def self.invariant(game_state_model)
     if game_state_model.player_turn_state == nil or game_state_model.player_turn_state > 2 or game_state_model.player_turn_state < 0 then raise MenuControllerContractError.new("Bad number of players") end  
     if game_state_model.players.count < 0 then raise MenuControllerContractError.new("Bad number of players") end 
   end
 
-  def post_alert_selection(alert_view)
+  def self.post_alert_selection(alert_view)
     if alert_view == nil then raise MenuControllerContractError.new("Alert didn't popup") end
   end
 
-  def pre_alert_selection(alert_view)
+  def self.pre_alert_selection(alert_view)
     if alert_view != nil then raise MenuControllerContractError.new("Can't double alert view") end
   end
 
--- connectall/lib/contracts/player_model_contracts.rb
+++ connectall/lib/contracts/player_model_contracts.rb
@@ -5,7 +5,7 @@ module PlayerModelContracts
     end
   end
 
-  def invariants?(model)
+  def self.invariant(model)
     if mode.player_num < 0 then raise PlayerModelContractError.new("player_num must be positive") end
     if mode.score < 0 then raise PlayerModelContractError.new("players score must be positive") end
   end
