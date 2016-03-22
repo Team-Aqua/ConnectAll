ce1b2219bdbdbc49c3bc1a1f3c7dbbe8fe57857d
anson-li <ansonli.94@gmail.com>
2016-03-22 16:38:29 -0600

    rip fonts

--- connectall/lib/views/game_views/game_header_view.rb
+++ connectall/lib/views/game_views/game_header_view.rb
@@ -57,7 +57,7 @@ module Views
       
       @tiles = @tilesClassic
 
-      @font = Gosu::Font.new(@window, "assets/fonts/Roboto-Bold.ttf", 18)
+      @font = Gosu::Font.new(@window, "assets/fonts/HN_Bd.ttf", 18)
       @question = BtnItem.new(@window, Gosu::Image.new("assets/images/btn_question_light.png"), 295, 10, 35, lambda { @controller.question_button_click }, Gosu::Image.new("assets/images/btn_question_light_click.png"))
       @cancel = BtnItem.new(@window, Gosu::Image.new("assets/images/btn_cancel_light.png"), 315, 10, 35, lambda { @window.start_menu }, Gosu::Image.new("assets/images/btn_cancel_dark.png", :tileable => false))
       @ico_two = Gosu::Image.new("assets/images/ico_2.png")
